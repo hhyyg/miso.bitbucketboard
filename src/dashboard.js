cb36706cfbbcb821da6c74ee1e5bfec1d8615e64
hhyyg <hiroka.yago@gmail.com>
2019-01-27 16:28:36 +0900

    Update

---
+++ src/dashboard.js
@@ -0,0 +1,54 @@
+/// <reference path="../node_modules/@types/jquery/index.d.ts" />
+
+const space = "&nbsp;";
+
+const data = window.__initial_state__;
+const pullRequests = data.dashboard.overview.pullRequests;
+const userName = data.global.currentUser.username;
+const notApprovedColor = "#d3dded";
+
+function getSpanDay(x, y) {
+  return Math.ceil((y - x) / 86400000);
+}
+
+function addOpendDaysText(pullRequestRowElement, created_on) {
+  if (!created_on) {
+    return;
+  }
+
+  const spanDay = getSpanDay(new Date(created_on), new Date());
+  $(pullRequestRowElement)
+    .find(".sc-chAAoq small")
+    .prepend(
+      `<span>opened${space}<bold>${spanDay}</bold>${space}days ago,${space}</span>`
+    );
+}
+
+function highLightingNotApproved(pullRequestRowElement, reviewing) {
+  const notApproved =
+    reviewing.participants.filter(value => {
+      return value.user.username === userName && value.approved === false;
+    }).length !== 0;
+
+  if (notApproved) {
+    $(pullRequestRowElement).css("background-color", notApprovedColor);
+  }
+}
+
+// pull requests to review
+$("section:eq(0) table tr[data-qa='pull-request-row']").each(
+  (pullRequestRowIndex, pullRequestRowElement) => {
+    const reviewing = pullRequests.reviewing[pullRequestRowIndex];
+
+    addOpendDaysText(pullRequestRowElement, reviewing.created_on);
+    highLightingNotApproved(pullRequestRowElement, reviewing);
+  }
+);
+
+// your pull requests
+$("section:eq(1) table tr[data-qa='pull-request-row']").each(
+  (pullRequestRowIndex, pullRequestRowElement) => {
+    const myPullRequest = pullRequests.authored[pullRequestRowIndex];
+    addOpendDaysText(pullRequestRowElement, myPullRequest.created_on);
+  }
+);
--- src/script.js
+++ src/script.js
@@ -1,13 +1,19 @@
 ﻿//@ts-check
 /// <reference path="../node_modules/@types/jquery/index.d.ts" />
 
-const userName = 'hhyyg';
-const notApprovedColor = '#d3dded';
-
 $(function () {
     page_overview();
-    page_pullrequests();
 });
+
+function injectScript(file, node) {
+    var th = document.getElementsByTagName(node)[0];
+    var s = document.createElement('script');
+    s.setAttribute('type', 'text/javascript');
+    s.setAttribute('src', file);
+    th.appendChild(s);
+}
+// @ts-ignore
+injectScript(chrome.extension.getURL('/dashboard.js'), 'body');
 
 async function page_overview() {
     if (location.pathname !== '/dashboard/overview') {
@@ -15,42 +21,5 @@ async function page_overview() {
     }
 
     // Click Read more button
-    // TODO: if exist click
     await $('p button').click();
-
-    $("table tr[data-qa='pull-request-row']")
-        .each((i, pullRequestRowElement) => {
-            // const url = $(pullRequestRowElement)
-            //     .find("a[data-qa='pull-request-row-link']")
-            //     .attr('href');
-
-            const userElement = $(pullRequestRowElement)
-                .find(`a[href='https://bitbucket.org/${userName}/']`);
-
-            const next = $(userElement).next();
-            if (next && next.length !== 0) {
-            } else {
-                $(pullRequestRowElement).css('background-color', notApprovedColor);
-            }
-        });
-
-}
-
-function page_pullrequests() {
-
-    if (location.pathname !== '/dashboard/pullrequests') {
-        return;
-    }
-
-    $("table tbody tr")
-        .each((i, pullRequestRowElement) => {
-            const approvalMark = $(pullRequestRowElement)
-                .find('.approval-link');
-
-            const approved = $(approvalMark).hasClass('approved');
-            if (!approved) {
-                $(pullRequestRowElement).css('background-color', notApprovedColor);
-            }
-        });
-
 }
